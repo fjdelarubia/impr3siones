65adf28786849752f4b595c0c2353ea4a2df4cb6
Javier de la Rubia <javier.jrt@gmail.com>
2021-11-11 07:47:38 +0100

    Arregladas las functions y resueltos comentarios de la PR

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
 import * as functions from 'firebase-functions';
+import { Order, OrderStatus } from '@impr3siones/models';
+import { leftPadding } from '@impr3siones/utils';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.region('europe-west3').https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+  const o: Order = { id: leftPadding(1, 0, 5), status: OrderStatus.Pending };
+  response.send(`Hello from Firebase! order: ${o.id}`);
+
 });
